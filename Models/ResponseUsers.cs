ff416ae6abfcae4d80c27c39500f86ec23b8c69b
Dat Nguyen <ntdat89.hn@gmail.com>
2021-04-16 16:48:17 +0700

    bug

---
+++ Models/ResponseUsers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorAntdProTemplate.Models
+{
+    public class User
+    {
+        public int Id { set; get; }
+        public string Email { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public string Avatar { set; get; }
+    }
+    public class Support
+    {
+        public string Url { set; get; }
+        public string Text { set; get; }
+    }
+    public class ResponseUsers
+    {
+        public int Page { set; get; }
+        public int PerPage { set; get; }
+        public int Total { set; get; }
+        public int TotalPage { set; get; }
+        public User[] Data { set; get; } = Array.Empty<User>();
+        public Support Support { set; get; }
+    }
+}
---
+++ Services/ResponseUsersService.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using blazorAntdProTemplate.Models;
+
+namespace blazorAntdProTemplate.Services
+{
+    public interface IResponseUsersService
+    {
+        Task<ResponseUsers> GetListUsersAsync();
+    }
+
+    public class ResponseUserService : IResponseUsersService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ResponseUserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ResponseUsers> GetListUsersAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<ResponseUsers>("https://reqres.in/api/users?page=2");
+        }
+    }
+}
