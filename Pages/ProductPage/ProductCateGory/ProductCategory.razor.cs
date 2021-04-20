9924b84dabe554ce41a29977b7dc76108aadb275
Dat Nguyen <ntdat89.hn@gmail.com>
2021-04-20 16:02:22 +0700

    loading 1

---
+++ Pages/ProductPage/ProductCateGory/ProductCategory.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorAntdProTemplate.Pages.ProductPage.ProductCateGory
+{
+    public partial class ProductCategory
+    {
+        
+    }
+}
