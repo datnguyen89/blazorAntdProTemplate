fb85b3eadc1ba3826e03ff48ac65c9f6da390743
Dat Nguyen <ntdat89.hn@gmail.com>
2021-04-22 09:25:07 +0700

    apploading int

--- Models/AppState.cs
+++ Models/AppState.cs
@@ -8,15 +8,22 @@ namespace blazorAntdProTemplate.Models
     public class AppState
     {
 
-        public bool AppLoading { get; private set; }
+        public int AppLoading { get; private set; }
         public AppState()
         {
-            AppLoading = false;
+            AppLoading = 0;
         }
         public event Action OnChangeAppLoading;
         public void SetLoading(bool isLoading)
         {
-            AppLoading = isLoading;
+            if (isLoading)
+            {
+                AppLoading += 1;
+            }
+            else
+            {
+                AppLoading -= 1;
+            }
             NotifyStateChanged();
         }
 
