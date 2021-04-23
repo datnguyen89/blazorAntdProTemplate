7738a8f296d381fe4b6a4e830655be88bccaf918
Dat Nguyen <ntdat89.hn@gmail.com>
2021-04-23 15:01:40 +0700

    cascading value by name

--- Pages/ProductPage/ProductListPage/ProductListPage.razor.cs
+++ Pages/ProductPage/ProductListPage/ProductListPage.razor.cs
@@ -11,7 +11,7 @@ namespace blazorAntdProTemplate.Pages.ProductPage.ProductListPage
 
     public partial class ProductListPage
     {
-        private int PageIndex = 2;
+        private int PageIndex = 1;
         private int PageSize = 6;
         private ProductList _data = new ProductList();
         [Inject] protected IProductService ProductService { get; set; }
@@ -20,7 +20,10 @@ namespace blazorAntdProTemplate.Pages.ProductPage.ProductListPage
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
+            appState.SetLoading(true);
             _data = await ProductService.GetCurrentProductAsync(PageIndex);
+            System.Threading.Thread.Sleep(1000);
+            appState.SetLoading(false);
         }
 
         private async void OnShowSizeChange(PaginationEventArgs args)
@@ -29,7 +32,7 @@ namespace blazorAntdProTemplate.Pages.ProductPage.ProductListPage
             PageIndex = args.Page;
             PageSize = args.PageSize;
             _data = await ProductService.GetCurrentProductAsync(PageIndex);
-            //System.Threading.Thread.Sleep(2000); 
+            System.Threading.Thread.Sleep(1000);
             appState.SetLoading(false);
             StateHasChanged();
         }
